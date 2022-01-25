f2faa2682c33fb70c017433296587cc1d15cb5ab
Laiho83 <samir.kabir83@gmail.com>
2022-01-25 18:38:12 +0100

    Add Validation

--- projects/resignation-period-calculator/js/main.js
+++ projects/resignation-period-calculator/js/main.js
@@ -18,6 +18,7 @@
       this.submitForm = document.getElementById('submitForm');
       this.result = document.getElementById('result');
       this.officeDaysLeft = document.getElementById('officeDay');
+      this.ErrorMsg = document.getElementsByClassName('error')[0];
     },
 
     bindEvents() {
@@ -25,10 +26,19 @@
     },
     
     render() {
+      this.ErrorMsg.classList.remove('active');
       this.result.value = this.dates.last.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
       this.officeDaysLeft.value = this.dates.office.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-      this.result.parentElement.classList.add('result-active');      
-      this.officeDaysLeft.parentElement.classList.add('office-active');      
+      this.result.classList.add('result-active');      
+      this.officeDaysLeft.classList.add('office-active');      
+    },
+
+    renderError() {
+      this.ErrorMsg.classList.add('active');
+      this.result.value = "";
+      this.officeDaysLeft.value = ""
+      this.result.classList.remove('result-active');      
+      this.officeDaysLeft.classList.remove('office-active'); 
     },
 
     renderInit() {
@@ -39,9 +49,14 @@
     onSubmit(e) {
       e.preventDefault();
       this.calculatePeriod().then(d => {
-        this.dates.last = this.calculateLastOfficialDate(d);
-        this.dates.office = this.calculateLastOfficeDate(this.dates.last);
-        this.render();
+        if (d.getDay() == 0 || d.getDay() == 6) {
+          this.renderError();        
+        } else {
+          this.dates.last = this.calculateLastOfficialDate(d);
+          this.dates.office = this.calculateLastOfficeDate(this.dates.last);
+          new Date(this.inputDate.value)
+          this.render();
+        }
       })
     },
 
@@ -57,14 +72,13 @@
       return d;
     },
 
-    calculateLastOfficeDate(days) {
-      let of = new Date(days);
-      let leaveDaysLeft = Math.ceil((Number(this.leaveDays.value)) / 12 * Number(days.getMonth()+1));
+    calculateLastOfficeDate(lastDay) {
+      let of = new Date(lastDay);
+      let leaveDaysLeft = Math.ceil((Number(this.leaveDays.value)) / 12 * Number(lastDay.getMonth()+1));
 
       calc(of, leaveDaysLeft);
       function calc(d, ldl) {
         if(ldl >= 0) {
-          console.log(d.getDay(), d.getDate());
           if (d.getDay() == 0 || d.getDay() == 6) {
             ldl = ldl;
           } else {
@@ -73,12 +87,14 @@
           d.setDate(d.getDate() - 1);
           calc(d, ldl);
         }
-      }
+      }      
+      
+      of.setDate(of.getDate() + 1);      
+      calc(of, this.extraDays.value)      
       of.setDate(of.getDate() + 1);
-      calc(of, this.extraDays.value)
-      of.setDate(of.getDate() + 1);
+      
       return of;
-    }
+    },
   };
 
   form.init();
