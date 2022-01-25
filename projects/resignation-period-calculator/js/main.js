84be1bc768a5d243dda9abde3ba7ad7fa94278cd
Laiho83 <samir.kabir83@gmail.com>
2022-01-25 07:16:52 +0100

    Add full calc functionalities.

--- projects/resignation-period-calculator/js/main.js
+++ projects/resignation-period-calculator/js/main.js
@@ -1,7 +1,9 @@
 (function() {
   var form = {
-    resignationDate: null,
-    officeDate: null,
+    dates: {
+      last: null,
+      office: null,
+    },
     init() {
       this.cacheDom();
       this.bindEvents();
@@ -12,6 +14,7 @@
       this.inputDate = document.getElementById('resDate');
       this.inputDays = document.getElementById('resDays');
       this.leaveDays = document.getElementById('leaveDays');
+      this.extraDays = document.getElementById('extra');
       this.submitForm = document.getElementById('submitForm');
       this.result = document.getElementById('result');
       this.officeDaysLeft = document.getElementById('officeDay');
@@ -22,8 +25,8 @@
     },
     
     render() {
-      this.result.value = this.resignationDate.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-      this.officeDaysLeft.value = this.officeDate.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+      this.result.value = this.dates.last.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+      this.officeDaysLeft.value = this.dates.office.toLocaleDateString('sl-SL', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
       this.result.parentElement.classList.add('result-active');      
       this.officeDaysLeft.parentElement.classList.add('office-active');      
     },
@@ -36,8 +39,8 @@
     onSubmit(e) {
       e.preventDefault();
       this.calculatePeriod().then(d => {
-        this.resignationDate = this.calculateLastOfficialDate(d);
-        this.officeDate = this.calculateLastOfficeDate(this.resignationDate);
+        this.dates.last = this.calculateLastOfficialDate(d);
+        this.dates.office = this.calculateLastOfficeDate(this.dates.last);
         this.render();
       })
     },
@@ -51,25 +54,34 @@
 
     calculateLastOfficialDate(d) {
       d.setDate(d.getDate() + Number(this.inputDays.value));
+      if(d.getDay() == 0) {
+        d.setDate(d.getDate() - 2);
+      } else if(d.getDay() == 6) {
+        d.setDate(d.getDate() - 1);
+      }
       return d;
     },
 
     calculateLastOfficeDate(days) {
       let of = new Date(days);
       let leaveDaysLeft = Math.ceil((Number(this.leaveDays.value)) / 12 * Number(days.getMonth()+1));
-      of.setDate(days.getDate() - leaveDaysLeft);
 
-
-      calc();
-
-      function calc() {
-        if(leaveDaysLeft > 1) {
-          of.setDate(of.getDate() - 1);
-          leaveDaysLeft = (of.getDay() === 0 || of.getDay() === 6) ? leaveDaysLeft : leaveDaysLeft - 1;
-          calc();
+      calc(of, leaveDaysLeft);
+      function calc(d, ldl) {
+        if(ldl >= 0) {
+          console.log(d.getDay(), d.getDate());
+          if (d.getDay() == 0 || d.getDay() == 6) {
+            ldl = ldl;
+          } else {
+            ldl = ldl - 1;            
+          }
+          d.setDate(d.getDate() - 1);
+          calc(d, ldl);
         }
       }
-
+      of.setDate(of.getDate() + 1);
+      calc(of, this.extraDays.value)
+      of.setDate(of.getDate() + 1);
       return of;
     }
   };
